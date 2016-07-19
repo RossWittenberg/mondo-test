4a7c9b9dad14e90bd09b0b74dc2ef23cf838ded3
q <ross.wittenberg@gmail.com>
2016-07-19 17:19:30 -0400

    heroku config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'heroku' }
+  config.action_mailer.default_url_options = { host: 'mondo-test.herokuapp.com' }
 
 end
